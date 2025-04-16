465318ae2fca96922f40dbce64734c920aa319a1
williamhdz94 <williamhernandez450@gmail.com>
2025-04-16 17:38:43 -0600

    Managing cache results country and capital

--- src/app/country/services/country.service.ts
+++ src/app/country/services/country.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { ICountriesResponse } from '../interfaces/ICountriesResponse';
-import { catchError, delay, map, Observable, of, throwError } from 'rxjs';
+import { catchError, delay, map, Observable, of, tap, throwError } from 'rxjs';
 import { CountryMapper } from '../mappers/country.mapper';
 import { ICountry } from '../interfaces/ICountry';
 
@@ -13,12 +13,21 @@ const API_URL = 'https://restcountries.com/v3.1';
 export class CountryService {
 
   private http = inject(HttpClient);
+  private queryCacheCapital = new Map<string, ICountry[]>();
+  private queryCacheCountry = new Map<string, ICountry[]>();
 
   searchByCapital( query: string ): Observable<ICountry[]> {
     query = query.toLocaleLowerCase();
 
+    if ( this.queryCacheCapital.has(query) ) {
+      return of( this.queryCacheCapital.get(query) ?? [] );
+    }
+
+    console.log('Llegando al servidor por el query', query);
+
     return this.http.get<ICountriesResponse[]>(`${ API_URL }/capital/${ query }`).pipe(
       map( ( data ) => CountryMapper.mapCountrieToCountryArray(data) ),
+      tap((countries) => this.queryCacheCapital.set(query, countries)),
       catchError( (err) =>  {
         return throwError(() => new Error('No se encontraron resultados'))
       })
@@ -28,8 +37,15 @@ export class CountryService {
   searchByCountry( query: string ): Observable<ICountry[]> {
     query = query.toLocaleLowerCase();
 
+    if ( this.queryCacheCountry.has(query) ) {
+      return of( this.queryCacheCountry.get(query) ?? [] );
+    }
+
+    console.log('Llegando al servidor por el query', query);
+
     return this.http.get<ICountriesResponse[]>(`${ API_URL }/name/${ query }`).pipe(
       map( ( data ) => CountryMapper.mapCountrieToCountryArray(data) ),
+      tap((countries) => this.queryCacheCountry.set(query, countries)),
       // delay(3000),
       catchError( (err) =>  {
         return throwError(() => new Error('No se encontraron resultados'))
